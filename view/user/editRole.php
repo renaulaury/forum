a65c57f6d81191012104e68128bd7e61a39bc954
Lily <112404773+Belywen@users.noreply.github.com>
2025-01-02 22:35:08 +0100

    mise en place des listes déroulantes de ban temp et définitif

--- view/user/editRole.php
+++ view/user/editRole.php
@@ -18,6 +18,33 @@ $listUsers = $result['data']['listUsers'];
             <option value="Banni Définitivement">Bannissement Définitif</option>
         </select>
 
+        <label for="banTemp">Bannissement temporaire</label>
+        <select name="banTemp">
+            <h2>Bannissement temporaire</h2>
+
+            <option value="Insultes ou propos offosant">Insultes ou propos offensants</option>
+            <option value="Spams ou pub non autorisée">Spam ou publicité non autorisée</option>
+            <option value="Perturbation intentionnelle">Perturbation intentionnelle</option>
+            <option value="Irrespect des règles de contenu">Non-respect des règles de contenu</option>
+            <option value="Irrespect d'un admin">Manque de respect envers les administrateurs</option>
+            <option value="Irrespect droits d'auteur">Non-respect des droits d’auteur</option>
+        </select>
+
+        <label for="banDef">Bannissement définitif</label>
+        <select name="banDef">
+            <h2>Bannissement Définitif</h2>
+
+            <option value="Harcèlement ou menaces">Harcèlement ou menaces</option>
+            <option value="Contenus illégaux">Contenus illégaux</option>
+            <option value="Usurpation d’identité">Usurpation d’identité</option>
+            <option value="Répétition d'infractions graves">Répétition d'infractions graves</option>
+            <option value="Violation grave des droits d’auteur">Violation grave des droits d’auteur</option>
+            <option value="Diffusion de contenus dangereux">Non-respect des droits d’auteur</option>
+        </select>
+
+        <label for="raison">Raison :</label>
+        <input type="text" name="raison" />
+
 
         <p class="button">
             <input class="validInput" type="submit" name="submit" value="Valider">
